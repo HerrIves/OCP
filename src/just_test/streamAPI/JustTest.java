7bd4e2c5a4146288e16ee88d90fa04485651bee4
HerrIves <spechtator@gmail.com>
2022-07-07 10:14:30 +0200

    added justTest/StreamAPI/JustTest allmatch FP164

--- src/just_test/streamAPI/JustTest.java
+++ src/just_test/streamAPI/JustTest.java
@@ -1,12 +1,15 @@
 package just_test.streamAPI;
 
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
-public class justTest {
+public class JustTest {
     static Predicate<String> p = ((x)-> x.length() > 0);
 
     public static void main(String[] args) {
         p.test("");
+
+        Stream.of("").allMatch(p);
     }
 }
 
